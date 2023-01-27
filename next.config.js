6f1432b55d389c4a53980993dbb9f6d67b638afb
Alain <ark2784@gmail.com>
2023-01-27 16:33:10 -0300

    configfile

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
-const path = require("path");
-const withSass = require("@zeit/next-sass");
+const path = require('path')
+const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
   cssModules: true,
-});
+})
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
-};
+}
+
+const nextConfig = {
+  images: {
+    domains: ['https://res.cloudinary.com', 'res.cloudinary.com'],
+  },
+}
+module.exports = {
+  images: {
+    domains: ['https://res.cloudinary.com', 'res.cloudinary.com'],
+  },
+}
+module.export = nextConfig
